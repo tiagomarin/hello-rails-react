841b54ff3460946e90d8b834e4060420817eda8a
Tiago Marin <tiago.lelinski@gmail.com>
2023-01-10 16:02:38 -0300

    add rails react and create App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="my-heading">Hello React Rails!</h1>
+    </main>
+  )
+}
+
+export default App;
